ec22f9d9c0a3adf1f6fcf10594be12fcd16f6c90
vedeshkin <evilfunt@gmail.com>
2016-12-29 21:59:41 +0300

    Changed POM file. Now it's a bit clear how to work with  Maven. Goal is to make a deployable POM without any headache.

--- src/main/java/com/github/vedeshkin/Log4JAndMaven/Main.java
+++ src/main/java/com/github/vedeshkin/Log4JAndMaven/Main.java
@@ -1,25 +1,22 @@
 package com.github.vedeshkin.Log4JAndMaven;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+
 import org.apache.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
+
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
+
 import java.net.URL;
 
 /**
  * Created by vedeshkin on 27.12.2016.
  */
-public class  Main{
+public class Main{
     private static final Logger log = Logger.getLogger(Main.class);
     public static void main(String[] args) throws Exception{
         log.info("Something here");
-        URL url = new URL("https://api.vk.com/method/users.get?user_ids=olyousya&fields=online");
+        URL url = new URL("https://api.vk.com/method/users.get?user_ids=olyousya&fields=online,last_seen");
         Gson gson = new Gson();
         VKResponse vkResponse = gson.fromJson(new InputStreamReader(url.openStream()),VKResponse.class);
         for (UserInfo u : vkResponse.getUserInfo())
